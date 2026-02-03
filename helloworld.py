dfba436611edb97617f87a76305b95e3ea02696c
Karl Etzel <151890630+karletzel@users.noreply.github.com>
2026-02-03 22:20:34 +0000

    added folder for csv output etc

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 print("hello world") 
 
 import csv
+import os
 from fitparse import FitFile
 
 def convert_fit_to_csv(fit_file_path, csv_file_path):
@@ -31,12 +32,33 @@ def convert_fit_to_csv(fit_file_path, csv_file_path):
                              data[record_data.name] = record_data.value
                writer.writerow(data.values())   
 
+def process_folder(folder_path):
+    output_folder = os.path.join(folder_path, 'csv_output')
+    os.makedirs(output_folder, exist_ok=True)
+    # List all FIT files in the folder
+    for file in os.listdir(folder_path):
+        if file.endswith(".FIT"):
+            fit_file_path = os.path.join(folder_path, file)
+            file_name_no_ext = os.path.splitext(file)[0]
+            csv_file_path = os.path.join(output_folder,file_name_no_ext + '.csv')
+            convert_fit_to_csv(fit_file_path, csv_file_path)
+            print(f"Converted {fit_file_path} to {csv_file_path}")
 
-fit_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\files\\tp-523890_20250912.FIT'
-csv_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\output_5_CSV.csv'
+# Replace with your folder path
+folder_path = '.'
+process_folder(folder_path)
+print("That's all folks.")
+
+
+#fit_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\files\\tp-523890_20250912.FIT'
+#csv_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\output_5_CSV.csv'
 #fit_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\TP10-22-16-27-16-835.FIT'
 #csv_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\output_Three_CSV.csv'
 #fit_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\TP10-31-13-56-25-487.FIT'
 #csv_file_path = 'C:\\Users\\kretz\\Documents\\pythonwork\\output_TWO_CSV.csv'
-convert_fit_to_csv(fit_file_path, csv_file_path)
+#fit_file_path = 'TP10-22-16-27-16-835.FIT'
+#fit_file_path = 'tp-523890_20250912.FIT'
+
+
+# convert_fit_to_csv(fit_file_path, csv_file_path)
 
