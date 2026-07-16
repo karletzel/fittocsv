2480f0644cd660cc87163af4a3cbf8bb951d8608
Karl Etzel <151890630+karletzel@users.noreply.github.com>
2026-07-15 19:08:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import functions_framework
 
 @functions_framework.cloud_event
 def process_fit_file(cloud_event):
-    print("Function is live! Waiting for FIT file...")
+    print("Function is super live! Waiting for FIT file...")
